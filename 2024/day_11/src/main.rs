53676b93b8e5975ce2719e39ce79f9adae84b0c1
Ben Wernicke <ben@wernicke.at>
2024-12-19 16:56:14 +0100

    multi threaded version still too slow need to tackle big O

--- 2024/day_11/src/main.rs
+++ 2024/day_11/src/main.rs
@@ -1,120 +1,132 @@
-mod problem {
-    use std::collections::HashMap;
-    fn stones_dump(stones: &Vec<usize>) {
-        for s in stones.iter() {
-            print!("{s} ");
-        }
-        println!();
-    }
+mod solution_1 {
 
-    enum RuleResult {
+    use std::sync::mpsc;
+    use std::thread;
+
+    #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+    enum Child {
         One(usize),
         Two(usize, usize),
     }
-    struct Blinker {
-        cash: HashMap<usize, RuleResult>,
+
+    fn is_even(d: usize) -> bool {
+        (d & 1) == 0
     }
 
-    impl Blinker {
-        fn apply_rules(&mut self, stone: usize, result: &mut Vec<usize>) {
-            match self.cash.get(&stone) {
-                Some(r) => {
-                    match r {
-                        RuleResult::One(v) => {
-                            result.push(*v);
-                        }
-                        RuleResult::Two(a, b) => {
-                            result.push(*a);
-                            result.push(*b);
-                        }
-                    }
-                    return;
-                }
-                None => (),
-            }
-
-            // first rule
-            if stone == 0 {
-                self.cash.insert(0, RuleResult::One(1));
-                result.push(1);
-                return;
-            }
-            // second rule
-            let num_digits = count_digits(stone);
-            if is_even(num_digits) {
-                let (upper, lower) = split_n_digits(stone, num_digits);
-                self.cash.insert(stone, RuleResult::Two(upper, lower));
-
-                result.push(upper);
-                result.push(lower);
-                return;
-            }
-            // third rule
-            result.push(stone * 2024);
-            self.cash.insert(stone, RuleResult::One(stone * 2024));
-        }
-
-        fn blink(&mut self, stones: &Vec<usize>, result: &mut Vec<usize>) {
-            for stone in stones {
-                self.apply_rules(*stone, result);
-            }
-        }
-
-        fn new() -> Blinker {
-            Blinker {
-                cash: HashMap::new(),
-            }
-        }
-    }
-
-    fn count_digits(u: usize) -> usize {
+    fn count_digits(d: usize) -> usize {
+        let mut ten = 10;
         let mut n = 1;
-        let mut d = 10;
-        while u / d != 0 {
-            d *= 10;
+
+        while d / ten != 0 {
             n += 1;
+            ten *= 10;
         }
         n
     }
 
-    /// Returns (upper_half, lower_half)
-    fn split_n_digits(num: usize, n: usize) -> (usize, usize) {
-        let mut d = 10;
-        for _ in 1..n / 2 {
-            d *= 10;
+    fn apply_rule(s: usize) -> Child {
+        if s == 0 {
+            return Child::One(1);
         }
-        (num / d, num % d)
+        let num_digits = count_digits(s);
+        if is_even(num_digits) {
+            let (upper, lower) = split_number(s, num_digits);
+            return Child::Two(upper, lower);
+        }
+        Child::One(s * 2024)
     }
 
-    fn is_even(u: usize) -> bool {
-        (u & 1) == 0
+    type Stack = Vec<(usize, usize)>;
+
+    fn blink_single(stack: &mut Stack, s: usize, g: usize) -> usize {
+        if g == 0 {
+            return 1;
+        }
+        let c = apply_rule(s);
+        match c {
+            Child::One(a) => {
+                stack.push((a, g - 1));
+            }
+            Child::Two(a, b) => {
+                stack.push((a, g - 1));
+                stack.push((b, g - 1));
+            }
+        }
+        return 0;
     }
 
-    pub fn solve(input: &str, n_blinks: usize) {
-        let mut stones: Vec<usize> = input
+    fn blink(stones: &Vec<usize>, generations: usize) -> usize {
+        let mut stack = Stack::new();
+        for s in stones {
+            stack.push((*s, generations));
+        }
+
+        let mut sum = 0;
+        while stack.len() > 0 && stack.len() < 22 {
+            let (s, g) = stack.pop().unwrap();
+            sum += blink_single(&mut stack, s, g);
+        }
+
+        let mut thread_handles = Vec::new();
+
+        let (sender, receiver) = mpsc::channel();
+
+        while stack.len() > 0 {
+            let stone = stack.pop().unwrap();
+            let mut stack = Stack::new();
+            stack.push(stone);
+            let sender = sender.clone();
+
+            thread_handles.push(thread::spawn(move || {
+                let mut sum = 0;
+                while stack.len() > 0 {
+                    let (s, g) = stack.pop().unwrap();
+                    sum += blink_single(&mut stack, s, g);
+                }
+
+                sender.send(sum).unwrap();
+            }))
+        }
+
+        for h in thread_handles {
+            h.join().unwrap();
+        }
+
+        drop(sender);
+
+        for val in receiver {
+            sum += val;
+        }
+
+        return sum;
+    }
+
+    fn split_number(d: usize, n: usize) -> (usize, usize) {
+        let mut ten = 10;
+        for _ in 1..n / 2 {
+            ten *= 10;
+        }
+        (d / ten, d % ten)
+    }
+
+    #[allow(unused)]
+    pub fn run() {
+        //let input = "125 17";
+        let input = &std::fs::read_to_string("input").unwrap();
+        let stones: Vec<usize> = input
+            .trim()
             .split_whitespace()
             .map(|n| n.parse().unwrap())
             .collect();
-        let mut result = vec![];
-        result.reserve(stones.len());
-        let mut blinker = Blinker::new();
 
-        for i in 0..n_blinks {
-            println!("Iteration: {i}\t\tstones.len(): {}", stones.len());
-            blinker.blink(&stones, &mut result);
-            let tmp = result;
-            result = stones;
-            stones = tmp;
-            result.clear();
-        }
-        println!("Solution 1: {}", stones.len());
+        let solution_1 = blink(&stones, 25);
+        println!("Solution 1: {}", solution_1);
+
+        let solution_2 = blink(&stones, 75);
+        println!("Solution 2: {}", solution_2);
     }
 }
 
 fn main() {
-    //let input = "125 17";
-    let input = &std::fs::read_to_string("input").unwrap();
-    let input = input.trim();
-    problem::solve(input, 25);
-    problem::solve(input, 75);
+    solution_1::run();
 }
