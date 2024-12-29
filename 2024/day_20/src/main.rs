bf0aaa3609f952f6f4a5824ac8464c3f319f8727
bendermeister <ben@wernicke.at>
2024-12-29 08:59:57 +0100

    i have more ideas what i am doing

--- 2024/day_20/src/main.rs
+++ 2024/day_20/src/main.rs
@@ -1,49 +1,98 @@
+use std::collections::HashSet;
 use std::collections::HashMap;
 
-fn solve(
-    map: &Vec<Vec<char>>,
-    seen: &mut HashMap<(isize, isize), usize>,
+enum Tile {
+    Free(usize),
+    Wall,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
     x: isize,
     y: isize,
-    cost: usize,
+}
+
+impl Point {
+    fn new(x: isize, y: isize) -> Point {
+        Point { x, y }
+    }
+    fn to_coords(&self) -> (usize, usize) {
+        (self.x.try_into().unwrap(), self.y.try_into().unwrap())
+    }
+    fn to_string(&self) -> String {
+        format!("({},{})", self.x, self.y)
+    }
+}
+
+impl std::ops::Add for Point {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl From<(usize, usize)> for Point {
+    fn from(p: (usize, usize)) -> Self {
+        Point {
+            x: p.0.try_into().unwrap(),
+            y: p.1.try_into().unwrap(),
+        }
+    }
+}
+
+fn neighbor_point_deltas() -> [Point; 4] {
+    [
+        Point::new(1, 0),
+        Point::new(-1, 0),
+        Point::new(0, 1),
+        Point::new(0, -1),
+    ]
+}
+
+fn explore_helper(
+    seen: &mut HashSet<Point>,
+    map: &mut Vec<Vec<Tile>>,
+    current: Point,
+    end: Point,
 ) -> Option<usize> {
-    match seen.get(&(x, y)) {
-        Some(val) if *val < cost => return None,
-        _ => (),
-    }
-    seen.insert((x, y), cost);
-
-    if x < 0 || y < 0 {
+    if seen.contains(&current) {
         return None;
     }
-    let x = x as usize;
-    let y = y as usize;
+    seen.insert(current);
 
-    if y >= map.len() || x >= map[y].len() {
-        return None;
+    let (x, y) = current.to_coords();
+    if current == end {
+        map[y][x] = Tile::Free(0);
+        return Some(1);
     }
-
     match map[y][x] {
-        'E' => return Some(cost),
-        '#' => return None,
+        Tile::Wall => return None,
         _ => (),
     }
-    let mut child = [None, None, None, None];
 
-    let x = x as isize;
-    let y = y as isize;
+    let neighbors: Vec<_> = neighbor_point_deltas()
+        .iter()
+        .map(|n| *n + current)
+        .map(|n| explore_helper(seen, map, n, end))
+        .filter(|n| match n {
+            Some(_) => true,
+            None => false,
+        })
+        .map(|n| n.unwrap())
+        .collect();
+    assert!(neighbors.len() == 1);
 
-    child[0] = solve(map, seen, x + 1, y, cost + 1);
-    child[1] = solve(map, seen, x - 1, y, cost + 1);
-    child[2] = solve(map, seen, x, y + 1, cost + 1);
-    child[3] = solve(map, seen, x, y - 1, cost + 1);
+    let cost = neighbors[0];
+    map[y][x] = Tile::Free(cost);
+    return Some(cost + 1);
+}
 
-    child.iter().fold(None, |acc, e| match (acc, *e) {
-        (None, None) => None,
-        (Some(val), None) => Some(val),
-        (None, Some(val)) => Some(val),
-        (Some(a), Some(b)) => Some(if a < b { a } else { b }),
-    })
+fn explore(map: &mut Vec<Vec<Tile>>, start: Point, end: Point) -> usize {
+    let mut seen = HashSet::new();
+    explore_helper(&mut seen, map, start, end).unwrap() - 1
 }
 
 fn main() {
@@ -63,42 +112,52 @@ fn main() {
 #.#.#.#.#.#.###
 #...#...#...###
 ###############";
-    let mut map: Vec<Vec<char>> = input
-        .trim()
-        .lines()
-        .map(|l| l.trim().chars().collect())
-        .collect();
 
-    let mut start_x = 0;
-    let mut start_y = 0;
-    'outer: for y in 0..map.len() {
-        for x in 0..map[y].len() {
-            if map[y][x] == 'S' {
-                map[y][x] = '.';
-                start_x = x as isize;
-                start_y = y as isize;
-                break 'outer;
+    let mut map = Vec::new();
+    let mut start = Point::new(0, 0);
+    let mut end = Point::new(0, 0);
+    let mut points = Vec::new();
+
+    for (y, line) in input.trim().lines().enumerate() {
+        let mut row = Vec::new();
+        for (x, c) in line.trim().chars().enumerate() {
+            match c {
+                '.' => {
+                    row.push(Tile::Free(0));
+                    points.push(Point::from((x, y)));
+                }
+                '#' => row.push(Tile::Wall),
+                'S' => {
+                    start = Point::from((x, y));
+                    points.push(Point::from((x, y)));
+                    row.push(Tile::Free(0));
+                }
+                'E' => {
+                    end = Point::from((x, y));
+                    points.push(Point::from((x, y)));
+                    row.push(Tile::Free(0));
+                }
+                _ => panic!("encountered unexpected tile"),
             }
         }
+        map.push(row);
     }
 
-    let mut seen = HashMap::new();
-    let base_time = solve(&map, &mut seen, start_x, start_y, 0).unwrap();
+    let base_time = explore(&mut map, start, end);
     println!("Base Time: {base_time}");
 
-    let solutions = Vec::new();
+    let mut result = Vec::new();
 
-    for y in 1..map.len() - 1 {
-        for x in 1..map[y].len() - 1 {
-            let x = x as isize;
-            let y = y as isize;
-
-            for dy in -1..=1 {
-                for dx in -1..=1 {
-                }
-            }
-
-        }
+    for point in points.iter() {
+        cheat(&mut result, &map, *point);
     }
 
+    let mut histo = HashMap::new();
+    for r in result.iter().filter(|r| **r < base_time) {
+        *histo.entry(r).or_insert(0) += 1;
+    }
+
+    for (k, v) in histo.iter() {
+        println!("{k}: {v}")
+    }
 }
